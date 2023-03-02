e480bab65563a1d96cef8dcbb993c1e7ac4e3d1e
Romaric <romaric.pascal@digital.cabinet-office.gov.uk>
2023-03-02 13:58:59 +0000

    Set up Sprockets to compile JavaScripts

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,4 @@
+// This loads a regular "script" file, like most projects would have already
+// in their application file one way or another (maybe more with require_tree)
+//= require non-es6/script
+
